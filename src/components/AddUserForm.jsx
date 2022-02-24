3b3dfd1258f2f12b0e798f9e211f6de6997ccf69
DarielOWL <darieldesosa@gmail.com>
2022-02-24 19:35:44 -0300

    Agregar usuarios y eliminar usuarios, andando correctamente

---
+++ src/components/AddUserForm.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useForm } from 'react-hook-form'
+
+const AddUserForm = (props) => {
+
+    //const {register, errors, handleSubmit} = useForm();
+    const {register, handleSubmit, formState: {errors}} = useForm()
+
+    const onSubmit = (data, e) => {
+        data.id = null
+        console.log(data)
+        props.addUser(data)
+        e.target.reset();
+    }
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            
+            <label>Name</label>
+                <input 
+                    type="text" 
+                    {...register("name", {required: {value: true, message: 'Campo obligatorio'}})}
+                />
+            <div>
+                {errors?.name?.message}
+            </div>
+            
+            <label>Username</label>
+                <input 
+                    type="text" 
+                    {...register("username", {required: {value: true, message: 'Campo obligatorio'}})}
+                />
+            <div>
+                {errors?.username?.message}
+            </div>
+            <button type="submit">Add new user</button>
+        </form>
+    );
+}
+ 
+export default AddUserForm;
+
+ 
